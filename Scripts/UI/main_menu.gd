358e1b9769c784d698edd03861ff0eed53e01455
Denis Ramirez <deensky@gmail.com>
2020-08-01 19:02:25 -0700

    remove old comments

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,25 +1,15 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var info = get_node("info")
-	#info.text = "Database size "+String(GameInstance.easy.size()) 
-	if GameInstance.error!=OK:
-		info.text = OS.get_locale()+" Error loadig json "+String(GameInstance.error)
-	#else:
-	#	info.text = String(GameInstance.error)+": No error, Database size "+String(GameInstance.ghost.size())+" lang "+OS.get_locale() 
+	#info.text = "Database size "+String(GameInstance.easy.size())me
 	
+	if GameInstance.error != OK:
+		info.text = OS.get_locale() + " Error loadig json " + String(GameInstance.error)
+	#else:
+	#	info.text = String(GameInstance.error) + ": No error, Database size " + String(GameInstance.ghost.size()) + " lang " + OS.get_locale()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_newgame_pressed():
 	get_tree().change_scene("res://Scenes/lab01.tscn")
