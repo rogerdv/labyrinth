8b41e83bb987b64b24aa3263d08eea8e10437052
Denis Ramirez <deensky@gmail.com>
2020-08-01 19:03:40 -0700

    format files and remove old comments

--- Scripts/ghost.gd
+++ Scripts/ghost.gd
@@ -1,19 +1,35 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 export(int) var chance = 5
 var PlayerVisible:bool = false
 var path
 onready var nav = get_node("../Navigation2D")
 const CHARACTER_SPEED = 110
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:	
+
+func _ready() -> void:
 	$Sprite.visible = false
 	$AnimationPlayer.play("idle")
+
+
+func _process(delta: float) -> void:
+	if PlayerVisible and !GameInstance.paused:
+		var r = randi()%1000
+		if r < chance:
+			$Sprite.visible = true
+
+	if $Sprite.visible and !GameInstance.paused:		#We are working
+		#print("The sprite is visible, check path")
+		path = nav.get_simple_path(global_position, get_node("../player").global_position)
+		if path.size()>0:
+			#there is a path to player
+			#print("There is path")
+			var walk_distance = CHARACTER_SPEED * delta
+			move_along_path(walk_distance)
+
+		#var space_state = get_world_2d().direct_space_state
+		#var result = space_state.intersect_ray(global_position, get_node("../player").global_position, [self])
 
 func _update_navigation_path(start_position, end_position):
 	# get_simple_path is part of the Navigation2D class
@@ -25,36 +41,17 @@ func _update_navigation_path(start_position, end_position):
 	path.remove(0)
 	set_process(true)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if PlayerVisible and !GameInstance.paused:
-		var r = randi()%1000
-		if r<chance:
-			$Sprite.visible = true
-
-	if $Sprite.visible and !GameInstance.paused:		#We are working
-		#print("The sprite is visible, check path")
-		path = nav.get_simple_path(global_position, get_node("../player").global_position)
-		if path.size()>0:
-			#there is a path to player		
-			#print("There is path")	
-			var walk_distance = CHARACTER_SPEED * delta
-			move_along_path(walk_distance)
-			
-		
-		#var space_state = get_world_2d().direct_space_state
-		#var result = space_state.intersect_ray(global_position, get_node("../player").global_position, [self])
 
 func move_along_path(distance):
 	var last_point = global_position
 	while path.size():
 		var distance_between_points = last_point.distance_to(path[0])
-		
+
 		# the position to move to falls between two points
 		if distance <= distance_between_points:
 			global_position = last_point.linear_interpolate(path[0], distance / distance_between_points)
 			return
-		
+
 		# the position is past the end of the segment
 		distance -= distance_between_points
 		last_point = path[0]
@@ -62,7 +59,7 @@ func move_along_path(distance):
 	# the character reached the end of the path
 	global_position = last_point
 	set_process(false)
-	
+
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	print("Entered visible area!!")
 	PlayerVisible = true
@@ -77,6 +74,6 @@ func _on_VisibilityNotifier2D_screen_exited() -> void:
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body.name=="player":
 		var panel = preload("res://UI/QuestionPanel.tscn").instance()
-		panel.mode = 2		
+		panel.mode = 2
 		panel.ghost = self
-		get_node("../CanvasLayer").add_child(panel)						
+		get_node("../CanvasLayer").add_child(panel)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var joystick = get_parent().get_node("CanvasLayer/Joystick/Joystick_Button")
+
+
 var UP:bool = false
 var DOWN:bool = false
 var LEFT:bool = false
@@ -12,12 +11,13 @@ var RIGHT:bool = false
 const bomb = "res://Scenes/bomb-projectile.tscn"
 var vec:Vector2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
+
 func _input(event):
-	#press events
+	# press events
 	if Input.is_action_just_pressed("ui_up"):
 		UP = true
 	if Input.is_action_just_pressed("ui_down"):
@@ -36,76 +36,81 @@ func _input(event):
 	if Input.is_action_just_released("ui_right"):
 		RIGHT = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
 	var coll
-	#mobile devices
+	
+	# mobile devices
 	if OS.has_touchscreen_ui_hint():
 		if !GameInstance.paused:
 			#get joystick value and normalize it
 			vec = joystick.get_value().normalized()
-			move_and_slide(vec*150)
+			move_and_slide(vec * 150)
+
 			#play the animation according to direction
-			if vec.x>0.1:
+			if vec.x > 0.1:
 				$AnimationPlayer.play("walk_right")
-			elif vec.x<-0.1:
+			elif vec.x < -0.1:
 				$AnimationPlayer.play("walk_left")
-			elif vec.y>0.1:
+			elif vec.y > 0.1:
 				$AnimationPlayer.play("walk_down")
-			elif vec.y<-0.1:
+			elif vec.y < -0.1:
 				$AnimationPlayer.play("walk_up")
-			elif vec==Vector2(0,0):
+			elif vec == Vector2(0,0):
 				$AnimationPlayer.stop()
-			if (get_slide_count()>0):
+			if (get_slide_count() > 0):
 				coll = get_slide_collision(0)
 			#print("Collided with: ", coll.collider.name)
 			#print(vec.normalized())
 				$RayCast2D.cast_to = vec* Vector2(30,30)
-	
+
 	if !GameInstance.paused:
-		#Keyboard based movement
+		# keyboard based movement
 		if UP:
 			#translate(Vector2(0,-150*delta))
 			move_and_slide(Vector2(0,-150), Vector2(0,0), true)
 			$AnimationPlayer.play("walk_up")
-			
-			if (get_slide_count()>0):
+
+			if (get_slide_count() > 0):
 				coll = get_slide_collision(0)
 				print("Collided with: ", coll.collider.name)
 				$RayCast2D.cast_to = Vector2(0,-30)
-			
-		if DOWN:		
+
+		if DOWN:
 			move_and_slide(Vector2(0,150), Vector2(0,0), true)
-			$AnimationPlayer.play("walk_down")		
+			$AnimationPlayer.play("walk_down")
 			if  (get_slide_count()>0):
-				#coll = get_slide_collision(0)			
+				#coll = get_slide_collision(0)
 				$RayCast2D.cast_to = Vector2(0,30)
-		
+
 		if LEFT:
 			move_and_slide(Vector2(-150,0), Vector2(0,0), true)
 			$AnimationPlayer.play("walk_left")
-			
+
 			if get_slide_count()>0:
 				$RayCast2D.cast_to= Vector2(-25,0)
 		if RIGHT:
 			move_and_slide(Vector2(150,0), Vector2(0,0), true)
 			$AnimationPlayer.play("walk_right")
-			
-			if get_slide_count()>0:			
+
+			if get_slide_count()>0:
 				$RayCast2D.cast_to = Vector2(25,0)
-		
+
+
 func ToggleCollision(toggle:bool):
 	$CollisionShape2D.disabled = toggle
-	
+
+
 func ToggleCamera(toggle:bool):
 	$Camera2D.current = toggle
+
 
 func FireBomb():
 	var b = load(bomb).instance()
 	if OS.has_touchscreen_ui_hint():
 		b.dir = vec
 	else:
-		vec = Vector2(0,0)	
+		vec = Vector2(0,0)
 		if UP:
 			vec.y = -1
 		if DOWN:
@@ -116,6 +121,3 @@ func FireBomb():
 			vec.x = 1
 		b.dir = vec
 	get_parent().add_child(b)
-	
-	
-	
