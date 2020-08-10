b71006522cf63dd22e138be418b27fbba4a87b8f
Denis Ramirez <deensky@gmail.com>
2020-08-09 19:10:31 -0700

    format

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -2,22 +2,22 @@ extends Control
 
 
 func _ready():
-	var info = get_node("info")
-	#info.text = "Database size "+String(GameInstance.easy.size())me
-	
-	if GameInstance.error != OK:
-		info.text = OS.get_locale() + " Error loadig json " + String(GameInstance.error)
-	#else:
-	#	info.text = String(GameInstance.error) + ": No error, Database size " + String(GameInstance.ghost.size()) + " lang " + OS.get_locale()
+    var info = get_node("info")
+    #info.text = "Database size "+String(GameInstance.easy.size())me
+    
+    if GameInstance.error != OK:
+        info.text = OS.get_locale() + " Error loadig json " + String(GameInstance.error)
+    #else:
+    #	info.text = String(GameInstance.error) + ": No error, Database size " + String(GameInstance.ghost.size()) + " lang " + OS.get_locale()
 
 
 func _on_newgame_pressed():
-	get_tree().change_scene("res://Scenes/lab01.tscn")
+    get_tree().change_scene("res://Scenes/lab01.tscn")        
 
 
 func _on_quit_pressed():
-	get_tree().quit()
+    get_tree().quit()
 
 
 func _on_continue_pressed() -> void:
-	GameInstance.LoadGame()
+    GameInstance.LoadGame()
